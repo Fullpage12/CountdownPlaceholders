fe54026de5ed03ad216e9eb6c702c634ee99e1c5
oliver11443 <oliver11443@icloud.com>
2021-10-18 20:13:03 +0100

    Added a reload and placeholder command

--- src/main/java/me/fullpage/countdownplaceholders/CountdownPlaceholders.java
+++ src/main/java/me/fullpage/countdownplaceholders/CountdownPlaceholders.java
@@ -1,18 +1,19 @@
 package me.fullpage.countdownplaceholders;
 
 import me.clip.placeholderapi.PlaceholderAPI;
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.logging.Level;
 
-public final class CountdownPlaceholders extends JavaPlugin {
+public final class CountdownPlaceholders extends JavaPlugin implements CommandExecutor {
 
     private static CountdownPlaceholders instance;
+    private static PapiHook papiHook;
 
     public static CountdownPlaceholders getInstance() {
         return instance;
@@ -23,31 +24,55 @@ public final class CountdownPlaceholders extends JavaPlugin {
         // Plugin startup logic
         instance = this;
 
-        File configf = new File(this.getDataFolder(), "config.yml");
-        if (!configf.exists()) {
-            configf.getParentFile().mkdirs();
-            this.saveResource("config.yml", false);
-        }
-        FileConfiguration config = new YamlConfiguration();
-        try {
-            config.load(configf);
-        } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
+        saveDefaultConfig();
+        this.getCommand("countdownplaceholders").setExecutor(this);
 
-        final PapiHook papiHook = new PapiHook();
-        if (!papiHook.isRegistered() && papiHook.register())
+        papiHook = new PapiHook();
+        if (!papiHook.isRegistered() && papiHook.register()) {
             getLogger().log(Level.INFO, "Successfully registered placeholders!");
-
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        final PapiHook papiHook = new PapiHook();
         PlaceholderAPI.unregisterPlaceholderHook(papiHook.getIdentifier());
     }
 
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender.isOp() || sender.hasPermission("countdownplaceholders.admin")) {
+            if (args.length == 0) {
+                sendMessage(sender, "&cIncorrect usage, try /countdownplaceholders <reload, placeholders>");
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("reload")) {
+
+                reloadConfig();
+                PlaceholderAPI.unregisterPlaceholderHook(papiHook.getIdentifier());
+                papiHook.register();
+
+                sendMessage(sender, "&aSuccessfully reloaded the plugin!");
+            } else if (args[0].equalsIgnoreCase("placeholders")) {
+                sendMessage(sender, "&6Countdown Placeholders:");
+                for (String key : getConfig().getKeys(false)) {
+                    String placeholder = "%countdown_" + key + "%";
+                    sendMessage(sender, "&e" + placeholder + (sender instanceof OfflinePlayer ? " (" + PlaceholderAPI.setPlaceholders((OfflinePlayer) sender, placeholder) + ")" : ""));
+                }
+
+            }
+        } else {
+            sendMessage(sender, "&cInsufficient Permission.");
+            return true;
+        }
+        return true;
+    }
+
+    private void sendMessage(CommandSender sender, String msg) {
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+    }
 
 }
